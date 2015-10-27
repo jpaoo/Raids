6742feaa0b05aceff7a7789713854cc75a4f3acb
jp_ao <jp_ao@hotmail.com>
2015-10-27 21:17:42 +0000

    Fixes para Cloud9

--- Modelo/util.php
+++ Modelo/util.php
@@ -1,10 +1,24 @@
+
 <?php
 function connect(){
-	$servername = "localhost";
+
+
+	/*$servername = "localhost";
 	$username = "root";
 	$password = "";
 	$dbname = "avientame";
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	$conn = mysqli_connect($servername, $username, $password, $dbname);*/
+	
+	$servername = "localhost";
+    $username = "jp_ao";
+    $password = "";
+    $database = "avientame";
+    $dbport = 3306;
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database, $dbport);
+	
+	
 	if ($conn->connect_error) {
 		die("Falló la conexión a la base de datos");
 	}
