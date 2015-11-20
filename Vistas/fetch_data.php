8731fc8c7e13da206690305f4127f26fe250c786
Ral15 <a00512318@itesm.mx>
2015-11-20 16:11:04 -0600

    Super mejora a registro auto

--- Vistas/fetch_data.php
+++ Vistas/fetch_data.php
@@ -1,21 +1,20 @@
 <?php
 
-echo "jajajajajaj";
-   if(isset($_POST['get_option']))
-   {
+  include_once("../Modelo/util.php");
+
+   if(isset($_POST['get_option'])) {
+
      $conn = connect();
 
-
      $make = $_POST['get_option'];
-     $sql = "SELECT model FROM modelos WHERE make = '$make'"
-     echo "hahahahahaha";
+     $sql = "SELECT DISTINCT model FROM modelos WHERE make = '$make' ORDER BY model";
      $find=mysqli_query($conn,$sql);
-     while($row=mysqli_fetch_array($find))
-     {
-       echo "<option>".$row['model']."</option>";
+     $s = "asdf";
+     while($row=mysqli_fetch_array($find)) {
+       $s = $s."<option>".$row['model']."</option>";
      }
-
-     exit;
+    echo $s;
+    return s;
    }
 
 ?>
